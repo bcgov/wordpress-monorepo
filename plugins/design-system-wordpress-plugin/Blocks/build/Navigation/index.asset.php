394fbc43c064720e782193e9b442d2bfbae5e819
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2025-01-31 14:49:11 -0800

    fixes

--- plugins/design-system-wordpress-plugin/Blocks/build/Navigation/index.asset.php
+++ plugins/design-system-wordpress-plugin/Blocks/build/Navigation/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '56aac65843965ccee562');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '39c46883683dcbae57a6');
