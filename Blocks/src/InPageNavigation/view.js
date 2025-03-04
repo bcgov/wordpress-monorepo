d1fdce573433f91d80e98c7a64c6a00bcec27cbf
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2025-03-04 15:29:48 -0800

    init of block

---
+++ Blocks/src/InPageNavigation/view.js
@@ -0,0 +1,11 @@
+/**
+ * In-Page Navigation Block Frontend JavaScript
+ *
+ * Handles the interactive functionality of the In-Page Navigation block:
+ *
+ * @since 1.0.0
+ */
+
+document.addEventListener('DOMContentLoaded', function () {
+
+});
