02aa5397d3bca82a5b8f878befd8b48a26bd45e0
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2025-03-12 09:06:09 -0700

    added new design system block category

---
+++ Blocks/build/InPageNavigation/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '6e725e2d9b83e90aad91');
---
+++ Blocks/build/InPageNavigation/view.js
@@ -0,0 +1,16 @@
+/******/ (() => { // webpackBootstrap
+/*!**************************************!*\
+  !*** ./src/InPageNavigation/view.js ***!
+  \**************************************/
+/**
+ * In-Page Navigation Block Frontend JavaScript
+ *
+ * Handles the interactive functionality of the In-Page Navigation block:
+ *
+ * @since 1.0.0
+ */
+
+document.addEventListener('DOMContentLoaded', function () {});
+/******/ })()
+;
+//# sourceMappingURL=view.js.map
