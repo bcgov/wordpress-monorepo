0e6a27699d7b60dda07c27af8b0a4db50c5b29f6
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2025-03-18 11:26:24 -0700

    added QOL for loading the breadcrumb smoother

--- Blocks/src/BreadCrumb/save.js
+++ Blocks/src/BreadCrumb/save.js
@@ -1,36 +1,11 @@
 import { useBlockProps } from '@wordpress/block-editor';
 
-export default function save({ attributes }) {
+export default function save() {
 	const blockProps = useBlockProps.save();
-	const { dividerType, currentAsLink } = attributes;
-
+	
 	return (
 		<div {...blockProps}>
-			<div className="dswp-block-breadcrumb__container">
-				{attributes.grandParentTitle && attributes.grandParentUrl && (
-					<>
-						<a href={attributes.grandParentUrl}>{attributes.grandParentTitle}</a>
-						<span className="separator">{dividerType === 'chevron' ? ' > ' : ' / '}</span>
-					</>
-				)}
-				
-				{attributes.parentTitle && attributes.parentUrl && (
-					<>
-						<a href={attributes.parentUrl}>{attributes.parentTitle}</a>
-						<span className="separator">{dividerType === 'chevron' ? ' > ' : ' / '}</span>
-					</>
-				)}
-				
-				{attributes.currentTitle && (
-					currentAsLink && attributes.currentUrl ? (
-						<a href={attributes.currentUrl} className="current-page-link">
-							{attributes.currentTitle}
-						</a>
-					) : (
-						<span className="current-page">{attributes.currentTitle}</span>
-					)
-				)}
-			</div>
+			<div className="dswp-block-breadcrumb__container"></div>
 		</div>
 	);
 }
