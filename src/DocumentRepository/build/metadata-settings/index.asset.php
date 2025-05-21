6a720442b8eb269d7d78ab626dad2d296a5e173c
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2025-05-21 07:59:09 -0700

    added build all

--- src/DocumentRepository/build/metadata-settings/index.asset.php
+++ src/DocumentRepository/build/metadata-settings/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '1314aadbbfb6da855bbf');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'ef2c766c6fef031b6f46');
