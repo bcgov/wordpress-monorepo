bc2d9da5df05de71ff739ecf412489b954021831
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2025-05-26 10:14:18 -0700

    fixed build

--- Blocks/build/Navigation/index.asset.php
+++ Blocks/build/Navigation/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'db2983f30a5e899edb7d');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'cf86705edc78e6d6feab');
