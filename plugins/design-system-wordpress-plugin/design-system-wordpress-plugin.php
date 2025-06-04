716789eafb63553f071484a1ca95c92cd9750513
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2025-06-04 14:32:35 -0700

    removed init for doc repo

--- plugins/design-system-wordpress-plugin/design-system-wordpress-plugin.php
+++ plugins/design-system-wordpress-plugin/design-system-wordpress-plugin.php
@@ -186,6 +186,6 @@ $auto_anchor_settings->init();
 // Initialize InPageNav.
 $in_page_nav = new InPageNav();
 
-// Initialize the DocumentRepository.
-$document_repository = new DocumentRepository();
-$document_repository->init();
+// Initialize the DocumentRepository. Bring back when MVP.
+// $document_repository = new DocumentRepository();
+// $document_repository->init();
